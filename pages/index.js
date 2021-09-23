53f3741d7d0a78a16075c16599b13c25ead3d6cf
Hlulani Baloyi <hlullyr@gmail.com>
2021-09-23 23:38:52 +0200

    Added tailwind configurations

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="">
-    This is the first page
+    <div>
+      <Head>
+        <title>Music App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <p>Added Tailwind</p>
     </div>
   )
 }
